a8e1cfef1ebf13ecce0ddf982347abf9290f56c3
Luanderson Alves <luaoderson@gmail.com>
2023-09-29 17:57:05 -0300

    feat: exists word done

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    found = list()
+    for file in instance:
+        occurrences = list()
+        for i, line in enumerate(file["linhas_do_arquivo"]):
+            if word in line.lower():
+                occurrences.append({"linha": i + 1})
+
+        if len(occurrences):
+            found.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+    return found
 
 
 def search_by_word(word, instance):
