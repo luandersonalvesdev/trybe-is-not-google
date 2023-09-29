15641c73f370d7bf43802d638932fc2334dbf173
Luanderson Alves <luaoderson@gmail.com>
2023-09-29 17:20:45 -0300

    feat: tests priority queue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,24 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
+
+
+MOCK_FILES = [
+    {"qtd_linhas": 9},
+    {"qtd_linhas": 2},
+    {"qtd_linhas": 8},
+    {"qtd_linhas": 4}
+]
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+
+    for file in MOCK_FILES:
+        priority_queue.enqueue(file)
+
+    assert priority_queue.dequeue() == MOCK_FILES[1]
+
+    assert priority_queue.search(0) == MOCK_FILES[3]
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        priority_queue.search(len(priority_queue))
