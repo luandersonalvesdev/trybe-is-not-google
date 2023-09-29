2d6d666b933cfbc1c969f1ef5ebc1ecda137a12f
Luanderson Alves <luaoderson@gmail.com>
2023-09-29 01:04:31 -0300

    feat: txt importer done

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file[len(path_file) - 4:] != '.txt':
+        return sys.stderr.write('Formato inválido\n')
+
+    if not os.path.isfile(path_file):
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+
+    with open(path_file, "r") as file:
+        file_opened = [linha.rstrip('\n') for linha in file.readlines()]
+        return file_opened
